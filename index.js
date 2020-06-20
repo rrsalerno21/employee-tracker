cf4bdc1133c7768aee6e1861892ceaa18d677c88
Rocky <rrsalerno21@gmail.com>
2020-06-20 09:56:26 -0700

    added start fn and colors npm package

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const inquirer = require('inquirer');
 const cTable = require('console.table');
 const config = require('./private/config');
 const util = require('util');
+const colors = require('colors');
 
 function makeDb() {
     const connection = mysql.createConnection(config);
@@ -22,4 +23,62 @@ function makeDb() {
 };
 
 const db = makeDb(config);
+
+async function start() {
+    const startResponse = await inquirer.prompt(
+        [
+            {
+                type: 'list',
+                name: 'operation',
+                message: 'What would you like to do?',
+                choices: [
+                    'View All Employees',
+                    'View All Employees by Department',
+                    'View All Employees by Manager',
+                    'Add Employee',
+                    'Remove Employee',
+                    'Update Employee Role',
+                    'Update Employee Manager',
+                    'View All Roles',
+                    'Add Role',
+                    'Remove Role'
+                ]
+            }
+        ]
+    );
+
+    switch (startResponse.operation) {
+        case 'View All Employees':
+            //
+            break;
+        case 'View All Employees by Department':
+            //
+            break;
+        case 'View All Employees by Manager':
+            //
+            break;
+        case 'Add Employee':
+            //
+            break;
+        case 'Remove Employee':
+            //
+            break;
+        case 'Update Employee Role':
+            //
+            break;
+        case 'Update Employee Manager':
+            //
+            break;
+        case 'View All Roles':
+            //
+            break;
+        case 'Add Role':
+            //
+            break;
+        case 'Remove Role':
+            //
+            break;
+    }
+};
+
 
