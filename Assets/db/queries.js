13418854f090ede875999e5a37376ecf3d1f2422
Rocky <rrsalerno21@gmail.com>
2020-06-23 14:43:16 -0700

    added viewAllEmployeesByDepartment query

--- Assets/db/queries.js
+++ Assets/db/queries.js
@@ -14,6 +14,21 @@ const queries = {
 		LEFT JOIN employee AS mT ON (eT.manager_id = mT.id)
 		INNER JOIN role ON (eT.role_id = role.id)
 		INNER JOIN  department ON (department.id = role.department_id)`,
-	viewAllEmployeesByManager: ``,
+	viewAllEmployeesByDepartment: 
+	`SELECT
+		department.id AS 'Dept ID',
+    	department.name AS 'Department',
+		eT.first_name,
+		eT.last_name,
+		role.title,
+    	et.id AS 'Emp. #',
+		role.salary,
+		CONCAT(mT.first_name, ' ', mT.last_name) AS 'Manager'
+	FROM
+		employee AS eT
+	LEFT JOIN employee AS mT ON (eT.manager_id = mT.id)
+	INNER JOIN role ON (eT.role_id = role.id)
+	INNER JOIN  department ON (department.id = role.department_id)
+	ORDER BY department.name`,
 		
 }
