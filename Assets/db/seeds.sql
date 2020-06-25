ec83935d2b76420d3c3ec234c6ca19c64bfa3650
Rocky <rrsalerno21@gmail.com>
2020-06-25 14:34:57 -0700

    updated seed data

--- Assets/db/seeds.sql
+++ Assets/db/seeds.sql
@@ -1,20 +1,110 @@
-INSERT INTO employee
-VALUES (01, 'Rocky', 'Salerno', 01, null);
+-- DEPARTMENTS
 
-INSERT INTO employee
-VALUES (02, 'Hannah', 'Salerno', 01, 01);
+INSERT INTO department (name)
+VALUES ('Executive');
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Sam', 'Smith', 01, 02);
+INSERT INTO department (name)
+VALUES ('Finance and Accounting');
 
-INSERT INTO role
-VALUES (01, 'Software Engineer', 120000, 01);
+INSERT INTO department (name)
+VALUES ('Engineering');
 
-INSERT INTO role (title, salary, department_id)
-VALUES ('Sales Rep', 75000, 02);
+INSERT INTO department (name)
+VALUES ('Product');
 
-INSERT INTO department
-VALUES (01, 'Engineering');
+INSERT INTO department (name)
+VALUES ('Marketing');
 
 INSERT INTO department (name)
 VALUES ('Sales');
+
+INSERT INTO department (name)
+VALUES ('Customer Success');
+
+-- ROLES
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('CEO', 200000, 01);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('CFO', 175000, 02);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('Accountant', 90000, 02);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('VP of Engineering', 150000, 03);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('Software Engineer', 100000, 03);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('QA Engineer', 75000, 03);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('VP of Product', 150000, 04);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('Product Manager', 100000, 04);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('VP of Marketing', 120000, 05);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('Marketing Engineer', 120000, 05);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('VP of Sales', 120000, 06);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('SDR', 45000, 06);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('VP of Customer Success', 40000, 07);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('Account Manager', 50000, 07);
+
+-- EMPLOYEES
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Rocky', 'Salerno', 01, null);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Andrew', 'Beeler', 02, 01);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Brad', 'Smith', 03, 02);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Chris', 'Chang', 04, 01);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Dick', 'Ronald', 05, 04);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Erin', 'Hall', 06, 04);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Frank', 'Gomez', 07, 01);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Greg', 'Kennedy', 08, 07);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Harold', 'Donally', 09, 01);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Ian', 'Albert', 10, 09);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Jim', 'Yang', 11, 01);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Kim', 'Chang', 12, 11);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Lee', 'Gonzalez', 13, 01);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Robert', 'Sanchez', 14, 13);
